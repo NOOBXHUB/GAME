87b0b73b0e9a6ff5ee1cfae45024727225ea0200
NOOBHUB <73346855+NOOBXHUB@users.noreply.github.com>
2025-12-03 03:35:07 +0700

    Update Premium.lua

--- Premium.lua
+++ Premium.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NOOBHUBX/Game/refs/heads/main/Loading"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NOOBXHUB/GAME/refs/heads/main/Loading"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
